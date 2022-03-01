531579788d3e4424af70d14ea476f9f7e494a9b2
stevtang <stevenmtang1@gmail.com>
2022-03-01 15:08:13 -0800

    initial commit

---
+++ src/Carousel.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./Carousel.css";
+import Card from "./Card";
+
+
+/** Carousel: displays images and arrows to navigate through them
+ * 
+ * Props:
+ * - photos: array of {src, caption} objects
+ * - title: string describing the collection of images
+ * 
+ * State:
+ * - currCardIdx: integer for current card index
+ * 
+ * App --> Carousel --> Card
+ */
+ function Carousel({ photos, title }) {
+  const [currCardIdx, setCurrCardIdx] = useState(0);
+
+  const currCard = photos[currCardIdx];
+  const total = photos.length;
+
+  //Increments currCardIdx state by 1
+  function goForward() {
+    setCurrCardIdx(currCardIdx + 1);
+  }
+
+  return (
+    <div className="Carousel">
+      <h1>{title}</h1>
+      <div className="Carousel-main">
+        <i
+          className="fas fa-chevron-circle-left fa-2x"
+          onClick={goForward}
+        />
+        <Card
+          caption={currCard.caption}
+          src={currCard.src}
+          currNum={currCardIdx + 1}
+          totalNum={total}
+        />
+        <i
+          className="fas fa-chevron-circle-right fa-2x"
+          onClick={goForward}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
